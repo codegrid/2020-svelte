f8aab3df49d3aa8c29ba40cb885150b4db5a18b7
soto3 <sotomura@pxgrid.com>
2020-08-11 18:42:53 +0900

    add demo

---
+++ 6/double-handler/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+let count = 0
+let lastClicked = ''
+
+function onClickButton () {
+  count++
+}
+
+function logClick () {
+  lastClicked = new Date()
+}
+</script>
+
+<button type="button" on:click={onClickButton} on:click={logClick}>increment</button>
+<output>{count}</output>
+<div>最終クリック日時：{lastClicked}</div>
+
+<style>
+:global(html) {
+  height: 100%;
+}
+
+:global(body) {
+  font-size: 2rem;
+}
+
+button {
+  margin-right: 0.5em;
+  font-size: inherit;
+}
+
+div {
+  font-size: 1rem;
+}
+</style>
---
+++ 6/once-mod/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+let count = 0
+
+function onClickButton () {
+  count++
+}
+</script>
+
+<button type="button" on:click|once={onClickButton}>button</button>
+<output>{count}</output>
+
+<style>
+:global(html) {
+  height: 100%;
+}
+
+:global(body) {
+  font-size: 2rem;
+}
+
+button {
+  margin-right: 0.5em;
+  font-size: inherit;
+}
+
+div {
+  font-size: 1rem;
+}
+</style>
